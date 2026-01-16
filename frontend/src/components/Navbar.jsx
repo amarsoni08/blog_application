ff5ade8606a11cae0c3cb6add3c4fc9d166834d6
Amar Soni <amarsoni220673@acropolis.in>
2026-01-16 12:39:03 +0530

    Fix commented lines in Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -77,10 +77,10 @@ export default function Navbar() {
     // If messages are seen in chat window
     socket.on("message-seen", () => loadUnread());
 
-    // // Optional: whenever socket connects, reload unread
+    // Optional: whenever socket connects, reload unread
     socket.on("connect", loadUnread);
 
-    // // Poll every 5 sec as backup
+    // Poll every 5 sec as backup
     const interval = setInterval(loadUnread, 5000);
 
     return () => {
